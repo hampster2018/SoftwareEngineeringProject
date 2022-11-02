c0d70f601b96968c6b97ab58884cc6b9658721c6
ChowMeins <jasonchau4627@gmail.com>
2022-11-02 18:12:43 -0500

    Initial Skeleton made for the Accident monitor

--- Flask_App.py
+++ Flask_App.py
@@ -3,13 +3,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")  
-def home():
-    return render_template('index.html', utc_dt=datetime.datetime.utcnow())
+#@app.route("/")  
+#def home():
+#    return render_template('index.html', utc_dt=datetime.datetime.utcnow())
 
-@app.route("/<name>")
-def user(name):
-    return f"Hello {name}!"
+@app.route("/") # TO DO: Change to "/Accident-Monitor"
+def user():
+    return render_template('accidentMonitor.html')
 
 
 if __name__ == "__main__":
