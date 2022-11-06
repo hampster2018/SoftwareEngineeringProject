606a498fe04edb1c91395a0192736d25d9d9f546
abzaf <abdullah.zafar@outlook.com>
2022-11-06 14:50:07 -0600

    added incident report table

--- Flask_App.py
+++ Flask_App.py
@@ -13,7 +13,9 @@ def payment():
     total = sum(row[0] for row in fees)
     return render_template('payment.html', name="Eric Shields", fees=fees, total=total)
 
-
+@app.route("/ReportIncidents")
+def report_incidents():
+    return render_template('reportIncidents.html')
 
 if __name__ == "__main__":
     app.run()
