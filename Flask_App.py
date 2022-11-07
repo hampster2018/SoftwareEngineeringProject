d7477e6562b492d41b2e1057fd01c6d5c35987c9
ChowMeins <jasonchau4627@gmail.com>
2022-11-06 22:03:31 -0600

    update violation monitor
    violation monitor created

--- Flask_App.py
+++ Flask_App.py
@@ -5,14 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")  
 def home():
-    return render_template('index.html', utc_dt=datetime.datetime.utcnow())
-
-@app.route("/Payment")
-def payment():
-    fees = [(49, "chicken"), (31, "soup"), (100, "Awesome")]
-    total = sum(row[0] for row in fees)
-    return render_template('payment.html', name="Eric Shields", fees=fees, total=total)
-
+    return render_template('violationMonitor.html')
 
 if __name__ == "__main__":
     app.run()
