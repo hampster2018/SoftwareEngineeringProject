8b840f0718e9741c076e584bdbdca39b46361ddb
Eric Shields <shieldse01@yahoo.com>
2022-12-05 15:14:02 -0600

    Added required login and changed main menu ref

--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from flask import current_app as app
 from flask import render_template, redirect, url_for, request
-from flask_login import login_required, logout_user, current_user
+from flask_login import login_required, current_user
 
 from .roles import roles_required
 from .db import *
@@ -11,14 +11,18 @@ main_bp = Blueprint(
 )
 
 @main_bp.route("/")
+@login_required
 def home():
+    print(current_user.get_id())
     return render_template("mainMenu.html")
 
 @main_bp.route("/Map")
+@login_required
 def googleMaps():
     return render_template("map.html")
 
 @main_bp.route("/AccountPreferences")
+@login_required
 def accPreference():
     user = GetUserById()
     email = user['email']
@@ -26,27 +30,33 @@ def accPreference():
     return render_template("accountPreferences.html", email=email, name=name)
 
 @main_bp.route("/ReportIncidents")
+@login_required
 def reportIncidents():
     return render_template("reportIncidents.html")
 
 @main_bp.route("/ReportConditions")
+@login_required
 def reportConditions():
     return render_template("reportConditions.html")
 
 @main_bp.route("/VehicleResearch")
+@login_required
 def vehicleResearch():
     return render_template("vehicleResearch.html")
 
 @main_bp.route("/ViolationMonitor")
+@login_required
 def violationMonitor():
     return render_template("violationMonitor.html")
 
 @main_bp.route("/AccidentMonitor")
+@login_required
 def accidentMonitor():
     return render_template("accidentMonitor.html")
 
 ## The report issue page takes in a issue type by selection and renders issue submission
 @main_bp.route("/ReportAnIssue")
+@login_required
 def reportIssue():
     return render_template("reportAnIssue.html")    
 
@@ -54,6 +64,7 @@ def reportIssue():
 ## appropriate issue type. Will then take in a description and route to handle
 ## Issue Submission to process that data
 @main_bp.route("/issueSubmission/<issueNum>")
+@login_required
 def issueSubmission(issueNum):
     switch={
         1: 'GPS and Network',
@@ -69,29 +80,35 @@ def issueSubmission(issueNum):
 ## Takes in Data Here from the previous form and saves it using MakeIssue DB call
 ## Redirects to home page
 @main_bp.route("/handleIssueSubmission", methods=['POST', 'GET'])
+@login_required
 def handleIssueSubmission():
     formdata = request.form['issue']
     MakeAppIssue([formdata])
     return redirect(url_for('main_bp.home'))
 
 @main_bp.route("/ContactEmergency")
+@login_required
 def contactEmergency():
     return render_template("contactEmergency.html")
 
 @main_bp.route("/PublicTransport")
+@login_required
 def transportation():
     return render_template("publicTransport.html")
 
 @main_bp.route("/Settings")
+@login_required
 def settings():
     return render_template("settings.html")
 
 @main_bp.route("/TollFeeAdmin")
+@login_required
 def toll_fees():
     tolls = GetTolls()
     return render_template("tollFees.html", tolls=tolls)
 
 @main_bp.route("/test")
+@login_required
 def test():
     print("Car Crash number 2 is: " + GetIssue('Car Crash', 2)['name'])
     return render_template("publicTransport.html")
